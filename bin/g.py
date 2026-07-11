c1d01798e6c64e9726022f970d4bdefa1d543cce
Pat LaVarre <p.lavarre@ieee.org>
2026-07-10 20:21:42 -0700

    g.py: Solve cat <(g.py) |cat -  # git.py, no matter that Zsh infers </dev/null

--- bin/g.py
+++ bin/g.py
@@ -3,29 +3,47 @@
 """
 usage: g.py ...
 
-call git.py in place of g.py, or call |grep.py in place of |g.py
+call the colocated git.py in place of g.py, or call the |grep.py in place of |g.py
+
+quirks:
+  without pipelike stdin, calls git.py [--help] [--make-bin] [SHFILE] [SHWORD ...]
+  with pipelike stdin, calls |grep.py SHFILE [SHWORD ...]
+  test results found by calling with SHWORD but without a SHFILE of 'bin/g' don't much matter
 
 examples:
-    g.py  # git.py
-    echo |g.py  # grep.py
-    cat <(g.py)  # git.py
-    cat <(g.py) |cat -  # git.py or grep.py chosen by Shell 🙄
-    cat <(</dev/tty g.py) |cat -  # git.py
+  g.py  # git.py
+  echo |g.py  # grep.py
+  g.py <requirements.txt  # grep.py
+  g.py </dev/null  # git.py
+  cat <(g.py)  # git.py
+  cat <(g.py) |cat -  # git.py, no matter that Zsh infers </dev/null
 """
 
 import os
 import signal
+import stat
 import subprocess
 import sys
 
-shverb = "git.py" if sys.stdin.isatty() else "grep.py"  # sadly, Zsh <() turns Stdin Isatty False
-sys.argv[0] = shverb
+#
+
+
+ifstat = os.fstat(sys.stdin.fileno())
+os_devnull_rdev = os.stat(os.devnull).st_rdev
+inull = stat.S_ISCHR(ifstat.st_mode) and (ifstat.st_rdev == os_devnull_rdev)
+ipipelike = (not sys.stdin.isatty()) and (not inull)  # lets 'cat <(g.py) |' work at Zsh
+
+shverb = "grep.py" if ipipelike else "git.py"
+argv0 = os.path.join(os.path.dirname(__file__), shverb)
+
+argv = list(sys.argv)
+argv[0] = argv0
 
 
 assert int(0x80 + signal.SIGINT) == 130
 pass_fds = tuple(int(_) for _ in os.listdir("/dev/fd"))
 try:
-    run = subprocess.run(sys.argv, pass_fds=pass_fds)
+    run = subprocess.run(argv, pass_fds=pass_fds)
 except KeyboardInterrupt:
     if sys.platform == "darwin":
         print(file=sys.stderr)  # after "^" and "C" printed without a "\r\n"
