0346196741d32496ab24241190db313576c8db61
Pat LaVarre <p.lavarre@ieee.org>
2026-09-08 11:18:43 -0700

    sh/.exit.sh: Also show how read-without-clear of $? in the source, but don't deploy it

--- sh/.exit.sh
+++ sh/.exit.sh
@@ -1,3 +1,7 @@
 # sh/.exit.sh = show the Process Exit Return Code of the Last Process
 
-function .exit() { echo + exit $? >&2; }  # most classic Sh rejects .exit as 'not a valid id'
+function .exit() { echo + exit $? >&2; }
+
+# function .exit() { local rc=$?; echo + exit $rc >&2; return $rc; }  # reads without clearing
+
+# many classic Sh reject .exit as 'not a valid id' or as 'Syntax error: "(" unexpected'
